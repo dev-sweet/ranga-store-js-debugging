026cd01cb1e8239203cc89711c5e3b9308c861ae
dev-sweet <sweetali0520@gmail.com>
2021-09-13 20:35:57 +0600

    card design completed

--- js/app.js
+++ js/app.js
@@ -19,10 +19,10 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: ${product.price}</h2>
-      <h4>Rating : ${product.rating.rate}  <i class="fas fa-star"></i></h4>
-      <h5>Total Reviews : ${product.rating.count}</h5>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <h4>Rating : ${product.rating.rate}  <i class="fas fa-star" ></i></h4>
+      <h5>Total Review : ${product.rating.count}</h5>
+      <button class="btn btn-primary" onclick="addToCart(${product.id},${product.price})" id="addToCart-btn">add to cart</button>
+      <button id="details-btn" class="btn btn-warning">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
